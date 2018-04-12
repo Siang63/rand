49b17895c21dae9d1bba63522e258086de72e137
m3072\w401 <shdoiew@gmail.com>
2018-04-12 16:44:46 +0800

    隨機亂數

---
+++ rand/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for (int i =0;i<6;i++)
+            {
+                Console.WriteLine(rand.Next(1, 43));
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
